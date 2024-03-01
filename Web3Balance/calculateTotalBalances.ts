5ca755e824fdef49c67d24e5a2996d26ce1d0fe9
Vadim91200 <kamenets@hotmail.fr>
2024-02-29 22:39:10 -0700

    Add route for total web3 balances

---
+++ Web3Balance/calculateTotalBalances.ts
@@ -0,0 +1,18 @@
+const calculateTotalBalance = (json): number => {
+    let totalBalance = 0;
+
+    try {
+        // Lecture du fichier JSON
+        const cryptos = JSON.parse(json);
+
+        // Itération à travers chaque entrée du fichier JSON
+        for (const address in cryptos) {
+            const crypto = cryptos[address];
+            totalBalance += crypto.total; // Ajout de la balance totale de chaque cryptomonnaie
+        }
+    } catch (error) {
+        console.error('Erreur lors de la lecture du fichier JSON:', error);
+    }
+
+    return totalBalance;
+};
